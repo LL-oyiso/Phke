f21167b45a4277aceb26190b2b0d2acf8b4cec64
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-03 16:09:20 +0200

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,15 @@ import Manwhite from '../Maninwhiteshirt (1).jpeg'
 function Hero() {
   return (
     <>
-    <div className='w-full     h-[900px] bg-black'>
+    <div className='w-full h-[900px] bg-black'>
         <div className=' justify-around text-white p-4 max-w-[1500px] m-auto  '>
         <div className=' text-white absolute top-[22%]  max-w-[690px]  h-full '>
         <h1 className='font-bold text-[60px] font-[Inter]'>You’ve never experiened <span className='text-[#8F8F8F]'>texting like this before</span> </h1>
         <p className='text-[27px] font-semibold mt-10 font-[Inter] w-[524px] ' >Pioneering the Future of Real-Time Chat: Shaping the Next Generation of Conversations</p>
        </div>
-      <div className='flex  ml-auto  w-[600px] mt-32  '>
-      <img src={Manwhite} alt= "" className=' rounded-2xl w-[278px] h-[422px] mr-1   object-contain'  />
+    
+      <div className='flex  ml-auto  w-[600px] mt-32'>
+      <img  src={Manwhite} alt= "" className=' rounded-2xl w-[278px] h-[422px] mr-1   object-contain'  />
       <img src={Manblue} alt="" className=' rounded-2xl w-[278px] h-[422px] ml-2 object-contain' />
       </div>
         </div>
