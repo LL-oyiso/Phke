29a2fe119a00bade6be0cd57431fd48c5101e73f
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-08 08:17:51 +0200

    maincontent first page complete , gif for maincontent inserted.

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import gif from '../Chat.gif'
 
 export const MainContent = () => {
   return (
     <> 
-<div className='w-full h-[900px] bg-white'>
+<div className='w-full h-[900px] bg-white flex'>
+    
+    <div className='mt-44 ml-10' >
+    <img src = {gif} alt =" "></img>
+
+    </div>
         <div className=' justify-around text-black p-4 max-w-[1500px] m-auto items-center '>
-         <h1 className='font-black text-[55px] mt-10'>Reimaginging text</h1>
-         <p className='text-[30px] w-[650px] mt-10'>Introducing our groundbreaking chat app that reimagines the way you connect and communicate,<br></br><br></br>
+         <h1 className='font-black text-[55px] mt-2 ml-24'>Reimaging text</h1>
+         <p className='text-[30px] w-[650px] mt-8 ml-24'>Introducing our groundbreaking chat app that reimagines the way you connect and communicate,<br></br><br></br>
 
             offering innovative features for a more engaging and personalized chatting experience.<br></br><br></br>
 
