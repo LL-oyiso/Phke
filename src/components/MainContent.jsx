45a48f305ffdbbc35d4ba5a204a41a670488132f
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-08 17:17:23 +0200

    main section removoed

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -59,9 +59,7 @@ export const MainContent = () => {
     <div className='bg-[#0761B4] h-[600px] flex '>
         <img src={lock} alt =" "  className='w-[600px] object-contain'/>
         <div className=''>
-            <h1 className='font-bold text-white mt-28    ml-32 text-[55px] '>Express yourself securely</h1>
-            <p className='text-white text-[35px] ml-32 w-[750px]'>Proactively averting harm from the outset, granting enhanced choices and command, providing considerate reactions, and embracing your openness in a private, secure, and assured manner</p>
-        </div>
+            
 
     </div>
     </div>
