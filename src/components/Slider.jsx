442fa23225b44527b3bea41cec1f0003a60e8ccc
LL-oyiso <theo.tose.tt@gmail.com>
2023-11-15 17:25:06 +0200

    save carouself for css

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,15 +24,15 @@ export function Slider() {
         <div className='mt-20'>
           {/* iterate through this using a map */}
           {data.map((d) => (
-            <div className='bg-purple h-[450px] text-black rounded-xl'> 
-                <div className='rounded-t-xl'> 
+            <div className='bg-white h-[450px] text-black rounded-xl'> 
+                <div className=' h-56rounded-t-xl bg-indigo-500 flex justify-center items-center'> 
                    
                 </div>
 
-                <div>
-                    <p> {d.name}</p>
+                <div className='flex flex-col justify-center items-center gap-4 p-4'>
+                    <p className='text-xl font-semibold'> {d.name}</p>
                     <p>{d.review}</p>
-                    <button>Read More</button>
+                    <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
                 </div>
             </div>
           ))}
